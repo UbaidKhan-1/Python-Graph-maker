dea307b1227f4fd0580fe47862ed80d6d1901feb
UbaidKhan-1 <excalliberagentics@gmail.com>
2026-05-13 11:30:26 +0500

    Made fist and second versions of graph.py

---
+++ graph.py
@@ -0,0 +1,235 @@
+import random
+import math
+import pygame as pg
+
+pg.init()
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 720, 1500
+SCREEN = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen_color = "white"
+clock = pg.time.Clock()
+
+x_limit = 360
+y_limit= SCREEN_HEIGHT/4
+orig =[SCREEN_WIDTH/2, y_limit]
+STEP= 50
+dx = 0.5
+
+text_size = 20
+text_color = "black"
+font = pg.font.Font(None, text_size)
+font2 = pg.font.Font(None, 70)
+font3 = pg.font.Font("DancingScript-Bold.ttf", 100)
+
+
+x = -10
+initial_x = x
+goal = 10
+# function specific values
+points = []
+points1 = []
+
+#               ((((((((((((()))))))))))))
+
+point_size = 5
+point_color = "black"
+
+graph_line_color = "black"
+graph_line_thickness = 3
+Plane_line_color = "purple"
+scale_lines_color = "red"
+
+projection = True			
+display_y_coordinate = False
+projection_lines_color = "red"
+
+
+#                      (    functions    )
+
+def f(x):
+	y = (x**2)-(4*x)
+	return y
+	
+def f1(x):
+	y = x**2
+	return y
+		
+#((((((((((((((((((((()))))))))))))))))))))))	
+	
+class PLANE:
+	def __init__(self, origin, x_limit, y_limit):
+		self.origin = origin
+		self.x_limit = x_limit
+		self.y_limit= y_limit
+		
+	def draw(self):
+		pg.draw.circle(SCREEN, "black", (self.origin[0], self.origin[1]), 5)
+		pg.draw.line(SCREEN, Plane_line_color, (self.origin[0]-self.x_limit, self.origin[1]), (self.origin[0]+self.x_limit, self.origin[1]), 3)
+		pg.draw.line(SCREEN, Plane_line_color, (self.origin[0], self.origin[1]+self.y_limit), (self.origin[0],self.origin[1]-self.y_limit), 3)
+		
+		#Drawing scale lines and blitting text on y_axis
+		for i in range(int((self.y_limit)/STEP)):
+			#for positive
+			x1 = self.origin[0] - 5
+			x2 = self.origin[0] + 5
+			y = self.origin[1] - i*STEP
+			pg.draw.line(SCREEN, scale_lines_color, (x1,y), (x2,y), 3)
+			if i> 0:
+				text = font.render(str(i), True, text_color)
+				text_rect = text.get_rect(center = (x1-text_size, y))
+				SCREEN.blit(text, text_rect)
+				
+			#for negative
+			y = self.origin[1] + i*STEP
+			pg.draw.line(SCREEN, scale_lines_color, (x1,y), (x2,y), 3)
+			if i>0:
+				text = font.render(str(-i), True, text_color)
+				text_rect = text.get_rect(center = (x1-text_size, y))
+				SCREEN.blit(text, text_rect)
+	
+	
+	#Drawing scale lines and blitting text on x_axis
+		for i in range(int((self.x_limit*2)/STEP)):
+			#for positive
+			y1 = self.origin[1] - 5
+			y2 = self.origin[1] + 5
+			x = self.origin[0] + i*STEP
+			pg.draw.line(SCREEN, scale_lines_color, (x,y1), (x,y2), 3)
+			if i>0:
+				text = font.render(str(i), True, text_color)
+				text_rect = text.get_rect(center = (x, y1-text_size))
+				SCREEN.blit(text, text_rect)
+				
+			#for negative
+			x = self.origin[0] - i*STEP
+			pg.draw.line(SCREEN, scale_lines_color, (x,y1), (x,y2), 3)
+			if i > 0:
+				text = font.render(str(-i), True, text_color)
+				text_rect = text.get_rect(center = (x, y1-text_size))
+				SCREEN.blit(text, text_rect)
+	#(((((((((((((((((((((((((())))))))))))))))))))))))))
+	
+	
+	def plot(self, points):
+		x = 0
+		y = 1
+		for point in points:
+				
+			#projecting on x and y axis
+					if projection == True:
+						pg.draw.line(SCREEN, projection_lines_color, (self.origin[0]+point[x]*STEP, self.origin[1]-point[y]*STEP), (self.origin[0]+point[x]*STEP, self.origin[1]), graph_line_thickness)
+						pg.draw.line(SCREEN, projection_lines_color, (self.origin[0]+point[x]*STEP, self.origin[1]-point[y]*STEP), (self.origin[0], self.origin[1]-point[y]*STEP), graph_line_thickness)
+						
+						#only ploting the points that lie within the graph
+					if self.origin[1] - point[1]*STEP > self.origin[1] - self.y_limit and self.origin[1] - point[1]*STEP < self.origin[1] + self.y_limit:
+						pg.draw.circle(SCREEN, point_color, (self.origin[0]+point[x]*STEP , self.origin[1]-point[y]*STEP), point_size)
+						if display_y_coordinate:	
+							text = font.render(str(point[y]),True, text_color)
+							text_rect = text.get_rect(center = ((self.origin[0]+point[x]*STEP) -(text_size+5), self.origin[1]-point[y]*STEP))
+							SCREEN.blit(text, text_rect)
+						#drawing a line between each point, connecting the points together
+						if (points.index(point)+1) < len(points):
+								pg.draw.line(SCREEN, graph_line_color, (self.origin[0]+point[x]*STEP , self.origin[1]-point[y]*STEP ),(self.origin[0]+points[points.index(point)+1][x]*STEP , self.origin[1]-points[points.index(point)+1][y]*STEP), graph_line_thickness)
+					if run_count == 0:
+						pg.display.update()
+
+
+cartesian_plane = PLANE(orig, x_limit, y_limit)
+
+class Slider:
+	def __init__(self):
+		self.x = 0
+		self.y = orig[1] + y_limit
+		self.dx = 0
+		self.sliderwidth = 150
+		self.default_sliderx = self.x + SCREEN_WIDTH-10-self.sliderwidth
+		self.sliderx = self.default_sliderx
+		self.height = 50
+		self.previousx = self.default_sliderx
+		
+	def make_slider(self):
+		pg.draw.rect(SCREEN, "grey", (self.x + 10, self.y, SCREEN_WIDTH-20, self.height))
+		pg.draw.rect(SCREEN, "dark grey", (self.sliderx, self.y+2, 150, self.height-5))
+		
+	def detect_sliding(self, x, y):
+		if y > self.y and y < self.y + self.height:
+			if x != self.previousx:
+				if x < self.default_sliderx:
+					self.sliderx = x
+					self.dx = x - self.previousx
+				else:
+					self.sliderx = self.default_sliderx
+					self.dx = x - self.previousx
+				self.updateglobal()
+				self.previousx = x
+			else:
+				pass
+		else:
+			pass
+	
+	def updateglobal(self):
+		global STEP
+		STEP += self.dx * 0.05
+		
+slider = Slider()
+
+running = True
+run_count = 0
+while running:
+	clock.tick(60)
+	mx, my = pg.mouse.get_pos()
+	events = pg.event.get()
+	for event in events:
+		if event.type == pg.QUIT:
+			running = False
+	
+	SCREEN.fill(screen_color)
+	cartesian_plane.draw()
+	
+	text = font2.render("("+"X = " +str(int(initial_x))+ " - " + str(int(goal)) + ")", True, "black")
+	text2 = font3.render("f(x) = tan-¹(x)", True, "dark green")
+	
+	
+	text_rect = text.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT-(SCREEN_HEIGHT/8)))
+	text2_rect = text2.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT-(SCREEN_HEIGHT/2)+150))
+	
+	
+	SCREEN.blit(text, text_rect)
+	SCREEN.blit(text2, text2_rect)
+	
+	# finding all points
+	while True:
+		if x <= goal:
+		    try:
+		        y1 = f(x)
+		        y2 = f1(x)
+		        
+		        # Skip complex values explicitly
+		        if isinstance(y1, complex):
+		            x += dx
+		            continue
+		        points.append([x,y1])
+		        #points1.append([x, y2])
+		        x += dx
+		    except:
+		        x += dx
+		    pg.display.update()
+		    continue
+		break
+	
+	
+	
+	#ploting points
+	cartesian_plane.plot(points)
+	cartesian_plane.plot(points1)
+	slider.make_slider()
+	slider.detect_sliding(mx, my)
+	
+	run_count += 1
+	if run_count%(60*3) == 0:
+		x = random.randint(-10, 0)
+		initial_x = x
+		run_count = 0
+		points = []
+
+	pg.display.update()
---
+++ graph2.py
@@ -0,0 +1,237 @@
+import pygame
+import random
+import math
+pygame.init()
+clock = pygame.time.Clock()
+WIDTH, HEIGHT = 720, 1500
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# settings
+canvascolor = "white"
+plane_line_thickness = 3
+graph_line_thickness = 1
+graph_text_color = "dark green"
+scale_text_color = "black"
+plane_lines_color = "black"
+scale_line_color = "red"
+graph_text_size = 30
+scale_text_size = 20
+origin = [WIDTH//2, HEIGHT//4 + 100]
+x_limit = WIDTH/2.2
+y_limit = WIDTH/2
+x_step = 50
+y_step = 50
+point_size = 3
+dx = 0.5#use fractions with denominator 2 to avoid data misrepresentation in binary
+plot_range = (-10, 10)
+draw_line = True
+project_on_xaxis = False
+project_on_yaxis = False
+show_coordinates = False
+progressively_draw_graph = True
+
+#list of tuples containing functions to be graphed and their colors
+functions = [(lambda x: x**2 -1, "red"),
+					  (lambda x: x+1,  "purple")]
+#___________________________________
+	
+# Experimental circle point function	
+def getcirclepoints(radius, resolution):
+	points = []
+	for i in range(resolution+1):
+		angle = i * (360/resolution)
+		x = radius*math.sin(math.radians(angle))
+		y = radius*math.cos	(math.radians(angle))
+		point = (x, y)
+		points.append(point)
+	return points
+#_____________________________
+
+def draw_text(fnt, string,color,position, size):
+	font = pygame.font.SysFont(fnt, size)
+	text_surface = font.render(string, True, color)
+	text_rect = text_surface.get_rect(center=position)
+	SCREEN.blit(text_surface, text_rect)
+	
+#_______________________________
+
+
+#((((((((((((((((((((((((((()))))))))))))))))))))))))))
+class cartesian_plane:
+	def __init__(self, origin, x_limit, y_limit, x_step, y_step):
+		self.origin = origin
+		self.x_limit = x_limit
+		self.y_limit = y_limit
+		self.x_step = x_step
+		self.y_step = y_step
+		self.runtime = 0
+		
+	def draw_plane(self):
+		originx = self.origin[0]
+		originy = self.origin[1]
+		
+#     drawing axis lines
+		pygame.draw.line(SCREEN, plane_lines_color, (originx, originy-self.y_limit), (originx,originy+self.y_limit), plane_line_thickness)
+		
+		draw_text('Ariel', " +y ", graph_text_color, (originx, originy-self.y_limit-graph_text_size/2), graph_text_size)
+		draw_text('Ariel', " -y ", graph_text_color, (originx, originy+self.y_limit+graph_text_size/2), graph_text_size)
+		    
+		pygame.draw.line(SCREEN, plane_lines_color, (originx-self.x_limit, originy), (originx+self.x_limit, originy), plane_line_thickness)
+		
+		draw_text('Ariel', "  +x ", graph_text_color, (originx+self.x_limit+graph_text_size/2, originy), graph_text_size)
+		draw_text('Ariel', " -x", graph_text_color, (originx-self.x_limit-graph_text_size/2, originy), graph_text_size)
+		#_____________________________
+		
+#drawing scale lines
+         # for x-axis
+		for i in range(round(self.x_limit/self.x_step)):
+			#for positive
+			pygame.draw.line(SCREEN, scale_line_color, (originx+i*self.x_step, originy-5), (originx+i*self.x_step, originy+5), plane_line_thickness)
+			
+			# for negative
+			pygame.draw.line(SCREEN, scale_line_color, (originx-i*self.x_step, originy-5), (originx-i*self.x_step, originy+5), plane_line_thickness)
+			
+		#drawing scale text for x-axis
+			if i!=0:
+				draw_text('Ariel', str(-i), scale_text_color, (originx-i*self.x_step, originy-5-scale_text_size/2), scale_text_size)
+				draw_text('Ariel', str(i), scale_text_color, (originx+i*self.x_step, originy-5-scale_text_size/2), scale_text_size)
+			
+		#for y-axis
+		for i in range(int(self.y_limit/self.y_step)):
+			#for positive
+			pygame.draw.line(SCREEN, scale_line_color, (originx-5, originy-i*self.y_step), (originx+5, originy-i*self.y_step), plane_line_thickness)
+			
+			
+			# for negative
+			pygame.draw.line(SCREEN, scale_line_color, (originx-5, originy+i*self.y_step), (originx+5, originy+i*self.y_step), plane_line_thickness)
+			
+			# drawing scale text for y-axis
+			if i!=0:
+				draw_text('Ariel', str(i), scale_text_color, (originx+5+scale_text_size/2, originy-i*self.y_step), scale_text_size)
+				draw_text('Ariel', str(-i), scale_text_color, (originx+5+scale_text_size/2, originy+i*self.y_step), scale_text_size)
+#______________________________
+	
+		
+	def plot(self, functions, rnge):
+		for func, projection_color in functions:
+			points = []
+			x = rnge[0]
+			goal = rnge[1]
+			while x<=goal:
+				try:
+					y = func(x)
+					points.append((x, func(x)))
+					x+=dx
+				except:
+					x+=dx
+				
+			#plotting obtained points
+			originx = self.origin[0]
+			originy = self.origin[1]
+			#graph boundaries
+			left_bound=originx-self.x_limit
+			right_bound=originx+self.x_limit
+			top_bound=originy-self.y_limit
+			bottom_bound=originy+self.y_limit
+			for point in points:
+					#coordinates
+					x = originx + point[0]*self.x_step
+					y = originy - point[1]*self.y_step
+					
+					if right_bound>=x>=left_bound and bottom_bound>=y>=top_bound:# prevents the points outside of graph area from being plotted
+					
+						if show_coordinates:
+							if points.index(point)%(1/dx)==0:# shows coordinates (only for the points with whole number x coordinates
+								draw_text('Ariel', "("+str(round(point[0],1))+", "+str(round(point[1],1))+")", scale_text_color, (x, y-10), scale_text_size)
+					#________________________
+					
+					# projects on axis
+						if project_on_yaxis:
+							pygame.draw.line(SCREEN, projection_color, (x, y), (originx, y), graph_line_thickness)
+						if project_on_xaxis:
+							pygame.draw.line(SCREEN, projection_color, (x, y), (x, originy), graph_line_thickness)
+					#_______________________
+					
+
+						
+						if points.index(point)<len(points)-1:#prevents index out of range
+							x2= originx + points[points.index(point)+1][0]*self.x_step
+							y2= originy - points[points.index(point)+1][1]*self.y_step
+								
+							if draw_line and right_bound>=x2>=left_bound and bottom_bound>=y2>=top_bound: # prevents the line from overshooting past the last plotted point in the graph
+								pygame.draw.line(SCREEN, projection_color, (x, y), (x2, y2), graph_line_thickness)
+						
+						pygame.draw.circle(SCREEN, projection_color, (x, y), point_size)# drawing the points after line so they are drawn pn top of line but they are two levels above in if-structure
+														
+					if not self.runtime:
+						if progressively_draw_graph:
+							pygame.display.update()
+		self.runtime +=1
+
+
+
+# SCALE SLIDER
+class Slider:
+	def __init__(self, plane):
+		self.x = 0
+		self.y = origin[1] + y_limit + graph_text_size
+		self.dx = 0
+		self.sliderwidth = 150
+		self.default_sliderx = self.x + WIDTH-10-self.sliderwidth
+		self.sliderx = self.default_sliderx
+		self.height = 50
+		self.plane = plane
+		self.previousx = self.default_sliderx
+		
+	def make_slider(self):
+		pygame.draw.rect(SCREEN, "grey", (self.x + 10, self.y, WIDTH-20, self.height))
+		pygame.draw.rect(SCREEN, "dark grey", (self.sliderx, self.y+2, 150, self.height-5))
+		
+	def detect_sliding(self, x, y):
+		if y > self.y and y < self.y + self.height:
+			if x != self.previousx:
+				if x < self.default_sliderx:
+					self.sliderx = x
+					self.dx = x - self.previousx
+				else:
+					self.sliderx = self.default_sliderx
+					self.dx = x - self.previousx
+				self.updateglobal()
+				self.previousx = x
+			else:
+				pass
+		else:
+			pass
+	
+	def updateglobal(self):
+		self.plane.x_step += self.dx * (x_step/WIDTH)
+		self.plane.y_step += self.dx* (y_step/WIDTH)
+		
+		
+#((())))))(((((())))))(((((())))))(((((())))))(((((())))
+		
+
+def main():
+	plane = cartesian_plane(origin, x_limit, y_limit, x_step, y_step)
+	slider = Slider(plane)
+			
+	run = True
+	while run:
+		clock.tick(60)
+		mx,my = pygame.mouse.get_pos()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+				break
+				
+		
+		SCREEN.fill(canvascolor)
+		
+		slider.make_slider()
+		slider.detect_sliding(mx, my)
+		plane.draw_plane()
+		plane.plot(functions, plot_range)
+		pygame.display.update()
+		
+if __name__=="__main__":
+	main()
